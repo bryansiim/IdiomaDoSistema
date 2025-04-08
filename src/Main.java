9062ada4f46b1ece4e1c8d87503f679ffb6db9c4
Bryan José <bryanfreitasmg@gmail.com>
2025-04-07 21:11:55 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault();
+        System.out.print("O idioma do Sistema esta em ");
+        System.out.println(idioma);
+    }
+}
